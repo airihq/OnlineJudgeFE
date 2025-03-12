8c654fc8e9c80d76849bda34315f9973d079ef57
imappak <imappak@gmail.com>
2025-03-12 18:23:57 +0900

    sentry url 변경

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -16,7 +16,7 @@ const options = {
 }
 
 Raven
-  .config('https://6234a51e61a743b089ed64c51d2f6ea9@sentry.io/258234', options)
+  .config('https://bf2e8b409c8c8ea6942100d11236c5e2@o4508963448881152.ingest.us.sentry.io/4508963458187264', options)
   .addPlugin(RavenVue, Vue)
   .install()
 
